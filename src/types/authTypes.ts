e12509b72b9d77a0dbb9e7704a56505ac14c1d04
Guetouache Kada || قطـــ قادة ـــواش <41374785+KadaGuetouache@users.noreply.github.com>
2025-02-01 19:36:04 +0100

    Update authTypes file to include a password in type

--- src/types/authTypes.ts
+++ src/types/authTypes.ts
@@ -2,6 +2,7 @@ export type userType = {
   name?: string | null | undefined;
   email?: string | null | undefined;
   image?: string | null | undefined;
+  password?: string | null | undefined;
 } & {
   id: string;
   username: string;
